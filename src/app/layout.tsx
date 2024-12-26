b5624f6897e2579127a807843e9d026bdc04655d
navin <95286412+sahnavin123@users.noreply.github.com>
2024-12-27 02:23:26 +0530

    configured styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className}`}>
-        <AppRouterCacheProvider>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
             {children}
